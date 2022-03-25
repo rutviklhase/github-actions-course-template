23614936675b9816a2198e89bddfa3c55c08ab11
rutviklhase <102267184+rutviklhase@users.noreply.github.com>
2022-03-25 07:04:41 +0530

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -2,12 +2,12 @@ const Game = require('../src/game').default
 const fs = require('fs')
 
 describe('App', () => {
-  it('Contains the compiled JavaScript',  async(done) => {
+  it('Contains the compiled JavaScript', async (done) => {
     fs.readFile('./public/main.js', 'utf8', (err, data) => {
       expect(err).toBe(null)
       expect(data).toMatchSnapshot()
       done()
-     })
+    })
   })
 })
 
